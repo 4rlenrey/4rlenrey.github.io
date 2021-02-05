183365ebb0a8f9a756b1c18ff3a2aaa68aa90c14
4rlenrey <soc.paw.mruz@gmail.com>
2021-02-05 23:57:25 +0100

    text now work

--- scripts/about.js
+++ scripts/about.js
@@ -2,4 +2,21 @@ window.onload = (event) => {
 	var year = new Date().getFullYear()
 	document.getElementById("age").innerHTML = (year - 2003);
 	document.getElementById("codingtime").innerHTML = (year - 2019);
+
+	let arr = document.getElementsByClassName("logg")
+
+
+	for (let step = 0; step < 17; step++) {
+		arr[step].addEventListener("mouseover", function g() {
+			document.getElementById("dos").innerHTML = arr[step].getAttribute('id');;
+		});
+
+	}
+
+
 };
+
+function onhovr(name) {
+	document.getElementById("dos").innerHTML = name;
+}
+
